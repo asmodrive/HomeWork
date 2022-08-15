93c9c50d2293d9eb0695476734d3f8152bcd23d0
Vasilev Petr <mikaelson4ik@gmail.com>
2022-08-15 14:41:04 +0300

    Pictures

--- pictures/Program.cs
+++ pictures/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int allPictures = 52;
+
+int row = 3;
+
+int rowCount = allPictures / row;
+
+int extraPictures = allPictures % row; 
+
+Console.WriteLine($"Вы заполнили полностью {rowCount} строк, у вас остались свободные картинки {extraPictures}.");
