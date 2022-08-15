6a65d841bb65bc20c921a876f55467defbe874dd
Vasilev Petr <mikaelson4ik@gmail.com>
2022-08-15 14:22:07 +0300

    HomeWork-2

---
+++ Anketa/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Напишите ваше имя");
+string name = Console.ReadLine();
+
+Console.WriteLine("Напишите ваш знак зодиака");
+string zodiacSign = Console.ReadLine();
+
+Console.WriteLine("Напишите ваш возраст");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Напишите ваше место работы");
+string workPlace = Console.ReadLine();
+
+Console.WriteLine($"Вас зовут {name}. Ваш знак зодиака {zodiacSign}. Ваш возраст - {age}. Ваше место работы - {workPlace}.");
