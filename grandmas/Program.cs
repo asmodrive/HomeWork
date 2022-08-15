56ab8714eb9ecc8b941ef5cd6a112528d89da513
Vasilev Petr <mikaelson4ik@gmail.com>
2022-08-15 17:55:45 +0300

    grandmas

---
+++ grandmas/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите количество старушек в очереди:");
+uint grandmas = Convert.ToUInt32(Console.ReadLine());
+uint expectation = 10;
+uint totalTime = 0;
+totalTime = grandmas * expectation;
+Console.WriteLine($"Вы должны отстоять в очереди {totalTime/60} ч. {totalTime%60} минут");
