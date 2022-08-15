7410842d314603138e244fa58a08b89284957fc5
Vasilev Petr <mikaelson4ik@gmail.com>
2022-08-15 17:41:36 +0300

    gold-gems

--- gold-and-gems/Program.cs
+++ gold-and-gems/Program.cs
@@ -2,7 +2,11 @@
 uint goldCount = Convert.ToUInt32(Console.ReadLine());
 uint gemsCount = 0;
 uint gemsPrice = 36;
+
 Console.WriteLine($"Сколько криссталов вы хотите приобрести? Цена за 1 кристалл - {gemsPrice} золотых");
 uint gemsToBuy = Convert.ToUInt32(Console.ReadLine());
-Console.WriteLine($"Вы приобрели {gemsToBuy} штук, у вас {gemsCount} штук.");
+gemsCount += gemsToBuy;
+goldCount -= gemsToBuy*gemsPrice;
+Console.WriteLine($"Вы приобрели {gemsToBuy} штук, у вас суммарно {gemsCount} штук, у вас осталось {goldCount} золотых.");
+
 
