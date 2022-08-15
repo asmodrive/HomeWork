e9e2f47c4a31a12b7497f8beb2a7e78096585bc4
Vasilev Petr <mikaelson4ik@gmail.com>
2022-08-15 17:13:55 +0300

    permutation-of-variables

---
+++ gold-and-gems/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Добрый день, сколько у вас денег?");
+uint goldCount = Convert.ToUInt32(Console.ReadLine());
+uint gemsCount = 0;
+uint gemsPrice = 36;
+Console.WriteLine($"Сколько криссталов вы хотите приобрести? Цена за 1 кристалл - {gemsPrice} золотых");
+uint gemsToBuy = Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine($"Вы приобрели {gemsToBuy} штук, у вас {gemsCount} штук.");
+
--- permutation of variables/Program.cs
+++ permutation of variables/Program.cs
@@ -1,11 +1,8 @@
 ﻿string name = "Vasilev";
-
 string surname = "Petr";
-
+Console.WriteLine($"Ваше имя - {name}, ваша фамилия - {surname}");
 string clipboard;
-
 clipboard = name;
-
 name = surname;
-
 surname = clipboard;
+Console.WriteLine($"Ваше имя - {name}, ваша фамилия - {surname}");
