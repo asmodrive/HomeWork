ad0410e93a9de304b4c3a5d5862c28b6bae9eb27
Vasilev Petr <mikaelson4ik@gmail.com>
2022-08-19 20:18:58 +0300

    sequense

---
+++ sequence/Program.cs
@@ -0,0 +1,8 @@
+﻿int restrictiveNumber = 103;
+int sequenceStep = 7;
+int sequenseMember;
+
+for (sequenseMember = 5; sequenseMember < restrictiveNumber; sequenseMember +=sequenceStep)
+{
+    Console.WriteLine(sequenseMember);
+}
