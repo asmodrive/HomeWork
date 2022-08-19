82ceb80c8f29c84e2285f7d937201be1f8694704
Vasilev Petr <mikaelson4ik@gmail.com>
2022-08-19 18:35:46 +0300

    repeat

---
+++ operator/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Напишите выражение");
+
+string message = Console.ReadLine();
+
+Console.WriteLine("Установите количество раз которое выражение будет выводиться на экран.");
+
+int repeatCount = Convert.ToInt32(Console.ReadLine());
+
+for (int i =0; i<repeatCount; i++)
+{
+    Console.WriteLine(message);
+}
