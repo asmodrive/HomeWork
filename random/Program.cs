1fa485217b332a4617f0f44ae5036ed86646b66b
Vasilev Petr <mikaelson4ik@gmail.com>
2022-08-25 17:38:56 +0300

    randomda

--- random/Program.cs
+++ random/Program.cs
@@ -1,16 +1,18 @@
-﻿Random rand = new Random();
+﻿Random random = new Random();
 int randomNumber;
+int randomMin = 1;
+int randomMax = 100;
 
-    randomNumber = rand.Next(0,100);    
+    randomNumber = random.Next(randomMin,randomMax);    
     Console.WriteLine(randomNumber);
     Console.ReadKey();
     int sumOfNumbers = 0;
-
-
+    int multipleNumber1 = 3;
+    int multipleNumber2 = 5;
 
 for (int i = 1; i <= randomNumber; i++)
 {
-    if (i % 3 == 0 || i % 5 == 0)
+    if (i % multipleNumber1 == 0 || i % multipleNumber2 == 0)
     {
         sumOfNumbers += i;
     }
