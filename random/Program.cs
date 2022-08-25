8bf112dcd5e7ed284e7ba054f4757947b752d083
Vasilev Petr <mikaelson4ik@gmail.com>
2022-08-25 15:58:33 +0300

    random

---
+++ random/Program.cs
@@ -0,0 +1,18 @@
+﻿Random rand = new Random();
+int randomNumber;
+
+    randomNumber = rand.Next(0,100);    
+    Console.WriteLine(randomNumber);
+    Console.ReadKey();
+    int sumOfNumbers = 0;
+
+
+
+for (int i = 1; i <= randomNumber; i++)
+{
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        sumOfNumbers += i;
+    }
+}
+    Console.WriteLine(sumOfNumbers);
